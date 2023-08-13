1b7c16606c7849d1205f9335c093a9ca5e0a982b
pengin banget <yahoiyuki@gmail.com>
2023-08-13 16:54:22 +0700

    mulai awal

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Script to update current date
+const currentDate = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = currentDate.toLocaleDateString('id-ID', options);
+document.getElementById('current-date').textContent = "Pada hari " + formattedDate
