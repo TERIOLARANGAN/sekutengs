36412e59b5844771c4f6c2e6b423df703dca30ba
pengin banget <yahoiyuki@gmail.com>
2023-08-19 22:26:19 +0700

    mengubah semua

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+/*
 alert("MERDEKA!!!")
 // Script to update current date
 const currentDate = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const formattedDate = currentDate.toLocaleDateString('id-ID', options);
 document.getElementById('current-date').textContent = "Pada hari " + formattedDate
+*/
+
+        const thankYouButton = document.getElementById("thankYouButton");
+
+        thankYouButton.addEventListener("click", function() {
+            const currentDate = new Date();
+            const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+            const formattedDate = currentDate.toLocaleDateString("id-ID", options);
+
+            alert(`Terima kasih telah mengunjungi kami pada hari ${formattedDate}`);
+        });
+
+
