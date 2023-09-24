6dfdb5a0c96cc9f694b67a8a368321824ec116c3
pengin banget <yahoiyuki@gmail.com>
2023-09-24 21:33:35 +0700

    evaluate all

---
+++ js/script1.js
@@ -0,0 +1,8 @@
+        // Temukan elemen tombol dengan id "sosmed"
+        var button = document.getElementById("sosmed");
+
+        // Tambahkan event listener untuk menangkap klik pada tombol
+        button.addEventListener("click", function() {
+            // Arahkan pengguna ke tautan Instagram yang diinginkan
+            window.location.href = "https://vt.tiktok.com/ZSN175wUo/"; // Gantilah "USERNAME_ANDA" dengan nama pengguna Instagram Anda
+        });
