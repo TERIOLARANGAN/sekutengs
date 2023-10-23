0ac18f8f35f954942a2dbe66d9dbae60fc80e416
pengin banget <yahoiyuki@gmail.com>
2023-10-23 22:04:44 +0700

    nambah

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+        // Temukan elemen tombol dengan id "sosmed"
+        var button = document.getElementById("sosmed1");
+
+        // Tambahkan event listener untuk menangkap klik pada tombol
+        button.addEventListener("click", function() {
+            // Arahkan pengguna ke tautan Instagram yang diinginkan
+            window.location.href = "Penasaran melihat aksi mereka dalam mengikuti ANBK ini?"; // Gantilah "USERNAME_ANDA" dengan nama pengguna Instagram Anda
+        });
